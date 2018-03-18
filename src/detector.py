aa42660c461c63a2739764014d1cc0bdf9bfa3ba
ChristopherLu <luxiaoxuan555@gmail.com>
2018-03-18 16:45:49 +0000

    further clean

--- src/detector.py
+++ src/detector.py
@@ -65,6 +65,8 @@ def detect_faces(image, min_face_size=20.0,
     # early return if bounding_boxes is empty
     if not bounding_boxes:
         return bounding_boxes, []
+    else:
+        bounding_boxes = np.vstack(bounding_boxes)
 
     keep = nms(bounding_boxes[:, 0:5], nms_thresholds[0])
     bounding_boxes = bounding_boxes[keep]
